7fa4b1c47b4c1690f9ebfa0fcb550bd9f5e43e4f
Fabamise Adeolu <fabamiseadeolu@gmail.com>
2020-06-24 20:55:16 +0100

    commit2

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -13,7 +13,7 @@ function AboutPage(props) {
 
                 <p>I'm constantly learning new things, currently those things include gaining more experience with React, NodeJS, ExpressJS and Laravel</p>
 
-                <p>Currently I improve my skills by watching tutorials on YouTube and using resources like <a href="https://freecodecamp.org">freecodecamp</a> to further improve my knowledge</p>
+                <p>Currently I improve my skills by watching tutorials on YouTube and using resources like <a href="https://freecodecamp.org">freecodecamp</a> to further improve my knowledge and skills</p>
             </Content>
         </div>
     );
